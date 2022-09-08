f7db04350547e9b5550ea0bbaf7a953e7ae84d3e
TLA <tina.lacatis@mediatack.de>
2022-09-08 17:18:50 +0200

    finishing the game

--- game.js
+++ game.js
@@ -1,21 +1,34 @@
 var buttonColors = ["green", "red", "yellow", "blue"];
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0;
+
+  $(document).one("keydown", function() {
+    $("#title").text("Level " + level);
+    nextSequence();
+  });
 
 $(".btn").on("click", function() {
   var userChosenColor = $(this).attr("id");
   userClickedPattern.push(userChosenColor);
   playSound(userChosenColor);
   animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length - 1);
 });
 
 function nextSequence() {
+  userClickedPattern = [];
+
+  level++;
+  $("#title").text("Level " + level);
+
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
 
   $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
   playSound(randomChosenColor);
+
 }
 
 function playSound(name) {
@@ -25,8 +38,36 @@ function playSound(name) {
 
 function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
-
-  setTimeout(function () {
+  setTimeout(function() {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("succes");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(nextSequence(), 1000);
+    }
+  } else {
+    console.log("wrong");
+    $("#title").text("Game Over, Press Any Key to Restart!");
+    var wrong = new Audio("sounds/wrong.mp3");
+    wrong.play();
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    startOver(currentLevel);
+  }
+
+}
+
+function startOver(){
+  level = 0;
+  gamePattern = [];
+  $(document).one("keydown", function() {
+    $("#title").text("Level " + level);
+    nextSequence();
+  });
 }
